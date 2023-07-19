4ca83a442a86984bc79a5b9417445bfda454fd71
Matt H <matt10145h@gmail.com>
2023-07-19 14:14:30 -0400

    Add dynamic window resizing for video

--- GUI.py
+++ GUI.py
@@ -10,32 +10,26 @@ import cv2
 from ultralytics.yolo.utils.plotting import Annotator
 import torch.nn as nn
 import torch
-from torch import Tensor
-from typing import Type
 import torchvision
 from torchvision import transforms, models
-import math
-from PIL import Image
-import numpy as np
 from scipy.optimize import linear_sum_assignment
 from collections import defaultdict
 import time
+
 # Disables console logs to improve speed
 logging.disable(logging.INFO)
 
 colors = ['Beige', 'Black', 'Blue', 'Brown', 'Gold', 'Green', 'Grey', 'Orange', 'Pink', 'Purple', 'Red', 'Silver', 'Tan', 'White', 'Yellow']
 body_types = ['Convertible', 'Coupe', 'Minivan', 'SUV', 'Sedan', 'Truck', 'Van']
 
-prev_detections = defaultdict(dict)  # Dictionary to store previous frame's detections for each car ID
-prev_confidence_scores = defaultdict(dict)  # Dictionary to store previous frame's detections for each car ID
-
-car_counter = 0  # Counter for assigning unique IDs to cars
-threshold_distance = 50  # Threshold for matching previous and current detections
-confidence_threshold = 80.0
-
 color_transformer = transforms.Compose([
     transforms.Resize((64, 64)),
     transforms.ToTensor()
+])
+
+transformer = torchvision.transforms.Compose([
+    transforms.Resize(size=(int(224), int(224))),
+    transforms.ToTensor(),
 ])
 
 class NET(nn.Module):
@@ -87,25 +81,15 @@ class NET(nn.Module):
         x = x.view(x.size(0), -1)
 
         return self.fc(x)
-color_classifier = NET()
-
-color_classifier.load_state_dict(torch.load("./Models/color_model.pt"))
-
-# Identifies classes to be searched for that the model was trained on
-
-# Declares a transformer to make all images fit the input size of the trained model
-transformer = torchvision.transforms.Compose([
-    transforms.Resize(size=(int(224), int(224))),
-    transforms.ToTensor(),
-])
-
-# Transfer Learning Model
+    
+    # Transfer Learning Model
 classifier = models.resnet34()
 num_ftrs = classifier.fc.in_features
 classifier.fc = nn.Linear(num_ftrs, len(body_types))
-
-# Loads the model that is trained on our data
 classifier.load_state_dict(torch.load("./Models/car_model_TL.pt"))
+
+color_classifier = NET()
+color_classifier.load_state_dict(torch.load("./Models/color_model.pt"))
 
 def get_probs(output):
     probs = nn.functional.softmax(output, dim=1)
@@ -134,8 +118,7 @@ def predict(img):
 
     return str(colors[color_output]).title() + " " + str(body_types[output]) + " " + prob + "%"
 
-model = YOLO('./YOLOModels/yolov8n.pt')
-# Misc. constants
+
 const_title = "AutoSense"
 font = ""
 background = "#FFFFFF"
@@ -143,138 +126,165 @@ text_background = "#FFFFFF"
 
 WINDOW_NAME = "Video Scanner"
 
-# Set up GUI
-class GUI(tk.Tk):
-    # Select video ?
-    def show_frame(self):
-        global prev_detections, prev_confidence_scores, car_counter
-        self.cap = cv2.VideoCapture(self.file)
-        _, frame = self.cap.read()
-        # frame = cv2.flip(frame, 1)
-        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+# Create the tkinter application
+class VideoClassifierApp:
+    def __init__(self, root, video_source):
+        self.root = root
+        self.root.title("Video Classifier")
 
-        # global model
-        results = model.predict(img)
+        # self.config(background=background)
 
-        new_detections = defaultdict(dict)  # Dictionary to store current frame's detections for each car ID
-        new_confidence_scores = defaultdict(dict)
-        for r in results:
-            annotator = Annotator(frame)
-            boxes = r.boxes
+        self.title = tk.Label(text=const_title, background=text_background)
+        self.title.pack()
 
-            # Create a distance matrix between previous and current detections
-            num_prev_detections = len(prev_detections)
-            num_current_detections = len(boxes)
-            distance_matrix = np.zeros((num_prev_detections, num_current_detections))
-
-            for i, (prev_id, prev_detection) in enumerate(prev_detections.items()):
-                for j, box in enumerate(boxes):
-                    b = box.xyxy[0].tolist()
-                    current_center = np.array([(b[2] + b[0]) / 2, (b[3] + b[1]) / 2])
-                    prev_center = prev_detection['center']
-                    distance = np.linalg.norm(current_center - prev_center)
-                    distance_matrix[i, j] = distance
-
-            # Assign detections using the Hungarian algorithm
-            prev_indices, current_indices = linear_sum_assignment(distance_matrix)
-            used_current_indices = set()
-
-            for i, prev_idx in enumerate(prev_indices):
-                current_idx = current_indices[i]
-                if distance_matrix[prev_idx, current_idx] > threshold_distance:
-                    continue
-
-                used_current_indices.add(current_idx)
-                prev_id = list(prev_detections.keys())[prev_idx]
-                car_id = prev_id
-                new_detections[car_id] = prev_detections[prev_id]
-                new_confidence_scores[car_id] = prev_confidence_scores[prev_id]
-
-                # class_id = r.names[box.cls[0].item()]
-                # if str(class_id) == "car" or str(class_id) == "bus" or str(class_id) == "truck":
-                b = boxes[current_idx].xyxy[0].tolist()
-                annotator.box_label(b, new_detections[car_id]['label'])
-
-            # Add new detections as new cars
-            for j, box in enumerate(boxes):
-                if j not in used_current_indices:
-                    b = box.xyxy[0].tolist()
-                    car_id = str(car_counter)
-                    car_counter += 1
-                    im_pil = Image.fromarray(img)
-                    im_pil = im_pil.crop([round(x) for x in b])
-                    class_id = r.names[box.cls[0].item()]
-                    if str(class_id) == "car" or str(class_id) == "bus" or str(class_id) == "truck":
-                        # if float(new_detections[car_id]['label'].split(" ")[2].split('%')[0]) < 83.0
-                        c = predict(im_pil)
-                        
-                        new_detections[car_id] = {'label': c, 'center': np.array([(b[2] + b[0]) / 2, (b[3] + b[1]) / 2])}
-                        annotator.box_label(b, new_detections[car_id]['label'])
-                        # frame = cv2.rectangle(frame, b, (36,255,12), 1)
-                        # cv2.putText(frame, new_detections[car_id]['label'], b[0], cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-
-        prev_detections = new_detections  # Update the previous detections for the next frame
-
-        frame = annotator.result()
-        # cv2.imshow(WINDOW_NAME, frame)
-
-        # if cv2.waitKey(1) & 0xFF == ord(' '):
-        #     return
-
-        displayImg = Image.fromarray(frame)
-        imgtk = ImageTk.PhotoImage(image=displayImg)
-        self.display1.imgtk = imgtk #Shows frame for display 1
-        self.display1.configure(image=imgtk)
-        self.after(10, self.show_frame) 
-
-    def prompt_video(self):
-        self.file = filedialog.askopenfilename(initialdir="c:/Users/matthew.hui/Documents/AutoSense", title="Select Video", filetypes=(("mp4 files", "*.mp4"), ("all files", "*.*")))
-        print(self.file)
-        split = self.file.split('/')[-2:]
-        self.file_dir = "Current video selected: " + (".../" + split[0] + "/" + split[1])
-        self.selected_file.configure(text=self.file_dir)
-        # time.sleep(2.5)
-        self.show_frame()
-
-    def detect():
-        print("detecting")
-
-    def __init__(self):
-        tk.Tk.__init__(self)
-
-        self.config(background=background)
-        # canvas = tk.Canvas(self, width=800, height=300)
-        # canvas.pack()
-
-        title = tk.Label(text=const_title, background=text_background)
-        title.pack()
+        self.counter = 0
 
         self.file = ""
         self.selected_file = tk.Label(text="Current video selected: ", background=text_background)
         self.selected_file.pack()
 
-        initial_color = StringVar(self)
-        initial_color.set(colors[2]) # Default value of Blue
+        self.initial_color = StringVar()
+        self.initial_color.set(colors[2]) # Default value of Blue
 
-        initial_body = StringVar(self)
-        initial_body.set(body_types[2])
+        self.color_menu = OptionMenu(self.root, self.initial_color, *colors)
+        self.color_menu.pack()
 
-        color_menu = OptionMenu(self, initial_color, *colors)
-        color_menu.pack()
+        self.initial_body = StringVar()
+        self.initial_body.set(body_types[2])
 
-        body_menu = OptionMenu(self, initial_body, *body_types)
-        body_menu.pack()
+        self.body_menu = OptionMenu(self.root, self.initial_body, *body_types)
+        self.body_menu.pack()
 
-        # imageFrame = tk.Frame(self, width=600, height=500)
-        # imageFrame.pack()
-        self.display1 = tk.Label()
-        self.display1.pack()
+        self.video_source = video_source
+        self.model = YOLO('./YOLOModels/yolov8n.pt')
 
-        video_select = Button(self, text='SELECT VIDEO', command=self.prompt_video, background=text_background)
-        video_select.pack()
+        black = Image.open('C:/Users/matthew.hui/Documents/AutoSense/Utilities/blackBackground.png')
+        blacker = ImageTk.PhotoImage(black)
+        self.background = tk.Label(self.root, background="#000000")
+        # self.background.configure(image=ImageTk.PhotoImage(Image.open('C:/Users/matthew.hui/Documents/AutoSense/Utilities/blackBackground.png')))
+        self.background.pack(fill=tk.BOTH, expand=True)
 
-        start_detection = Button(self, text='BEGIN DETECTION', command=self.detect, background=text_background)
-        start_detection.pack()
+        self.video_select = Button(self.root, text='SELECT VIDEO', command=self.prompt_video, background=text_background)
+        self.video_select.pack()
 
-gui = GUI()
-gui.mainloop()
+        self.start_detection = Button(self.root, text='BEGIN DETECTION', command=self.detect, background=text_background)
+        self.start_detection.pack()
+
+        # Fullscreen
+        # root.attributes("-fullscreen", True)
+
+        self.prev_detections = defaultdict(dict)
+        self.car_counter = 0
+        self.threshold_distance = 50
+
+    def prompt_video(self):
+        self.file = filedialog.askopenfilename(initialdir="c:/Users/matthew.hui/Documents/AutoSense", title="Select Video", filetypes=(("mp4 files", "*.mp4"), ("all files", "*.*")))
+        split = self.file.split('/')[-2:]
+        self.file_dir = "Current video selected: " + (".../" + split[0] + "/" + split[1])
+        self.selected_file.configure(text=self.file_dir)
+        # time.sleep(2.5)
+        self.cap = cv2.VideoCapture(self.video_source)
+        self.update()
+
+    def detect():
+        print("detecting")
+
+    def get_frame(self):
+        ret, frame = self.cap.read()
+        if ret:
+            return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        else:
+            return None
+
+    # Pass in a PIL image
+    def resize_with_aspect_ratio(self, image, desired_height):
+        width, height = image.size 
+
+        # if width > max_width or height > max_height:
+        #     ratio = min(max_width / width, max_height / height)
+        #     new_width = int(width * ratio)
+        #     new_height = int(width * ratio)
+
+        ratio = width // height
+        new_width = ratio * desired_height
+
+        return image.resize((new_width, desired_height), Image.ANTIALIAS)
+        # return image
+
+    def update(self):
+        frame = self.get_frame()
+
+        if frame is not None:
+            img = Image.fromarray(frame)
+            results = self.model.predict(frame)
+
+            new_detections = defaultdict(dict)
+            for r in results:
+                annotator = Annotator(frame)
+                boxes = r.boxes
+
+                num_prev_detections = len(self.prev_detections)
+                num_current_detections = len(boxes)
+                distance_matrix = np.zeros((num_prev_detections, num_current_detections))
+
+                for i, (prev_id, prev_detection) in enumerate(self.prev_detections.items()):
+                    for j, box in enumerate(boxes):
+                        b = box.xyxy[0].tolist()
+                        current_center = np.array([(b[2] + b[0]) / 2, (b[3] + b[1]) / 2])
+                        prev_center = prev_detection['center']
+                        distance = np.linalg.norm(current_center - prev_center)
+                        distance_matrix[i, j] = distance
+
+                prev_indices, current_indices = linear_sum_assignment(distance_matrix)
+                used_current_indices = set()
+
+                for i, prev_idx in enumerate(prev_indices):
+                    current_idx = current_indices[i]
+                    if distance_matrix[prev_idx, current_idx] > self.threshold_distance:
+                        continue
+
+                    used_current_indices.add(current_idx)
+                    prev_id = list(self.prev_detections.keys())[prev_idx]
+                    car_id = prev_id
+                    new_detections[car_id] = self.prev_detections[prev_id]
+
+                    b = boxes[current_idx].xyxy[0].tolist()
+                    annotator.box_label(b, new_detections[car_id]['label'])
+
+                for j, box in enumerate(boxes):
+                    if j not in used_current_indices:
+                        b = box.xyxy[0].tolist()
+                        car_id = str(self.car_counter)
+                        self.car_counter += 1
+                        im_pil = Image.fromarray(frame)
+                        im_pil = im_pil.crop([round(x) for x in b])
+                        class_id = r.names[box.cls[0].item()]
+                        if str(class_id) == "car" or str(class_id) == "bus" or str(class_id) == "truck":
+                            c = predict(im_pil)
+
+                            new_detections[car_id] = {'label': c, 'center': np.array([(b[2] + b[0]) / 2, (b[3] + b[1]) / 2])}
+                            annotator.box_label(b, new_detections[car_id]['label'])
+
+                self.prev_detections = new_detections
+                frame = annotator.result()
+
+            # picsize = self.canvas.size
+            screen_width = int(root.winfo_width() // 1.5)
+            screen_height = int(root.winfo_height() // 1.5)
+            # processed_frame = self.resize_with_aspect_ratio(Image.fromarray(frame), 400)
+            processed_frame = Image.fromarray(frame).resize((screen_width, screen_height), Image.ANTIALIAS)
+            self.photo = ImageTk.PhotoImage(image=processed_frame)
+            
+            # self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
+            self.background.configure(image=self.photo, height=screen_height, width=screen_width)
+
+        self.root.after(20, self.update)
+
+    def close(self):
+        self.cap.release()
+        self.root.destroy()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = VideoClassifierApp(root, "C:/Users/matthew.hui/Documents/AutoSense _old/20230711_122653.mp4")
+    root.mainloop()
